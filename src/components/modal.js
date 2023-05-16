9e959cc210349f26ad659762c4af833316ec2c33
Roberto Herrera <betohr48@hotmail.com>
2023-05-16 13:54:50 -0600

    cambios

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,12 +1,21 @@
-import {Dropdown, Modal, Text, Container, Button} from "@nextui-org/react";
+import {Dropdown, Modal, Text, Container, Button, Input} from "@nextui-org/react";
 import React from "react";
+import dayjs from "dayjs";
+
 
 function ModalAgregar({toOpen, funcClose}) {
     const tipos = {
-        'puntual': 0,
-        'recurrente': 1,
+        'Puntual': 0,
+        'Recurrente': 1,
     }
+    const dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
     const [tipo, setTipo] = React.useState(new Set([Object.keys(tipos)[0]]));
+    const [dia, setDia] = React.useState(new Set([dias[0]]));
+    const [nombre, setNombre] = React.useState('');
+    const [descripcion, setDescripcion] = React.useState('');
+    const [hora, setHora] = React.useState('');
+    const [fecha, setFecha] = React.useState(dayjs().format('YYYY-MM-DD'));
+    const [duracion, setDuracion] = React.useState(0);
 
 
     return (
@@ -18,7 +27,7 @@ function ModalAgregar({toOpen, funcClose}) {
             onClose={funcClose}
         >
             <Modal.Header>
-                <Text h1 id={"modal-title"}>Agregar nuevo evento individual</Text>
+                <Text h3 id={"modal-title"}>Agregar nuevo evento individual</Text>
             </Modal.Header>
             <Modal.Body>
                 <Container display={'flex'} direction={"row"} justify={"space-between"} alignItems={"center"}>
@@ -47,7 +56,39 @@ function ModalAgregar({toOpen, funcClose}) {
                         </Dropdown.Menu>
                     </Dropdown>
                 </Container>
-                <Button onPress={()=> console.log([...tipo.keys()])}>Agregar</Button>
+                <Input label={"Nombre del evento"} bordered color={"primary"}
+                       onChange={(event)=>{setNombre(event.target.value)}}/>
+                <Input label={"Descripción del evento"} bordered color={"primary"}
+                        onChange={(event)=>{setDescripcion(event.target.value)}}/>
+                <Input label={"Hora del evento"} bordered color={"primary"} type={"time"}
+                        onChange={(event)=>{setHora(event.target.value)}}/>
+                {!!tipo.has('puntual') ? (
+                        <Input label={"Fecha del evento"} bordered color={"primary"} type={"date"}
+                        onChange={(event)=>{setFecha(event.target.value)}}/>) :
+                    (<Dropdown>
+                        <Dropdown.Button flat color={'primary'}>
+                            {dia}
+                        </Dropdown.Button>
+                        <Dropdown.Menu
+                            disallowEmptySelection={true}
+                            selectionMode={'single'}
+                            onSelectionChange={setDia}
+                        >
+                            {dias.map((option) => (
+                                <Dropdown.Item
+                                    key={option}
+                                    textValue={option}
+                                >
+                                    {option}
+                                </Dropdown.Item>))}
+                        </Dropdown.Menu>
+
+                    </Dropdown>)}
+                <Input label={"Duración del evento en minutos"} bordered color={"primary"} type={"number"}
+                        onChange={(event)=>{setDuracion(event.target.value)}}/>
+                <Button onPress={() => console.log([...tipo.keys()][0], [...dia][0], nombre, descripcion, hora, fecha, duracion)}>
+                    Agregar
+                </Button>
             </Modal.Body>
         </Modal>
     )
--- src/serverServices/selects.js
+++ src/serverServices/selects.js
@@ -1,6 +1,22 @@
-import {pool} from "@/server_services/connection";
+import {pool} from "connection";
+import {escape} from "mysql2";
 
-// async function getTareas() {
-//     const [tareas] = await pool.query('SELECT * FROM tareas');
-//     return tareas;
+async function getEventosInd(idUsuario){
+    const query = `Select * from tareas where id_usuario = `+ escape(idUsuario) + `;`;
+    const [eventos] = await pool.query(query);
+    return eventos;
+}
+
+
+// async function getSuscripciones(idUsuario) {
+//     const query = `Select * from suscripcion where id_usuario = `+ escape(idUsuario) + `;`;
 // }
+
+async function getEventosSuscripcion(idSuscripcion){
+    const query = `Select * from eventos_grupales where id_usuario = `+ escape(idUsuario) + `;`;
+    const [eventos] = await pool.query(query);
+    return eventos;
+}
+
+
+
